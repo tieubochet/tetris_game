78ed8b57104f850a257e84b14a0bfed50eafc65b
tieubochet <tao.nguyen712@gmail.com>
2025-08-06 10:19:00 +0700

    v1

--- src/hooks/useStage.ts
+++ src/hooks/useStage.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect } from 'react';
 import { createStage } from '../services/gameHelpers';
 import { Player, Stage, StageCell, TetrominoKey } from '../types';
 
