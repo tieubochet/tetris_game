13de4603fbef0cf9766a1f200f90dcc737f16d7d
tieubochet <tao.nguyen712@gmail.com>
2025-08-07 10:44:16 +0700

    v1.2

--- src/components/StartButton.tsx
+++ src/components/StartButton.tsx
@@ -7,7 +7,7 @@ interface StartButtonProps {
 const StartButton: React.FC<StartButtonProps> = ({ callback }) => {
   return (
     <button
-      className="w-full px-4 py-3 my-4 text-xl font-bold text-white bg-blue-600 border-4 border-blue-800 rounded-md hover:bg-blue-500 focus:outline-none transition-colors duration-200"
+      className="w-full px-4 py-2 text-lg font-bold text-white bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none transition-colors duration-200"
       onClick={callback}
     >
       Start Game
